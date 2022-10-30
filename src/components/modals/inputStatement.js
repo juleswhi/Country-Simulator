61668659b9870fdd5e11a192cf772d87a0c5d1fd
white <juliet.whitte@gmail.com>
2022-10-30 22:10:44 +0000

    Automatically Creates Categories/Channels

--- src/components/modals/inputStatement.js
+++ src/components/modals/inputStatement.js
@@ -1,3 +1,5 @@
+const { ChannelType, PermissionFlagsBits } = require("discord.js");
+const User = require("../../schemas/user");
 const mongoose = require("mongoose");
 
 module.exports = {
@@ -5,19 +7,38 @@ module.exports = {
     name: `inputStatement`,
   },
   async execute(interaction, client) {
-    await interaction.reply({
-      content: null,
-    });
+    try {
+      await interaction.reply({});
+    } catch (err) {}
   },
   async run(interaction, client) {
-    const paragraph = interaction.fields.getTextInputValue("statementInput").toLowerCase();
+    const paragraph = interaction.fields
+      .getTextInputValue("statementInput")
+      .toLowerCase();
 
     const statementsChannel = client.channels.cache.find(
       (channel) => channel.name === "statements"
     );
 
+    const guild = await client.guilds.cache.get("1032948591112765510");
+    console.log(`Gotten Guild`);
     statementsChannel.send(paragraph);
 
-    
+    const user = await User.findOne({
+      userName: interaction.user.tag,
+    });
+    if (
+      client.channels.cache.find(
+        (channel) =>
+          channel.type === ChannelType.GuildCategory &&
+          channel.name === `${user.Country}`
+      )
+    ) {
+      return;
+    }
+    guild.channels.create({
+      name: `${user.Country}`,
+      type: ChannelType.GuildCategory,
+    });
   },
 };
