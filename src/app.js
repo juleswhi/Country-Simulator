40735a252a68c922df1aa94ff26e51747923e57e
white <juliet.whitte@gmail.com>
2022-11-02 15:05:21 +0000

    Express Implemination

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ const { token, databaseToken } = process.env;
 const { connect } = require("mongoose");
 const CountryDataA = require("./commands/tools/stats.js");
 const User = require("./schemas/user");
+const express = require('express')
+const app = express()
+
+app.use(express.static("public"));
+
+app.get("/", function ( req, res )
+{
+  res.send("Hello World")
+})
+
 
 const {
   Client,
@@ -33,7 +43,7 @@ const Resources = JSON.parse(raw3);
 exports.Resources = Resources;
 
 // (async () => {
-//     const UserData = await User.find({
+  //     const UserData = await User.find({
 //         userName: interaction.user.tag,
 //       });
 //       const CDB = JSON.parse(UserData);
@@ -43,11 +53,11 @@ exports.Resources = Resources;
 const functionFolders = fs.readdirSync("./src/functions");
 for (const folder of functionFolders) {
   const functionFiles = fs
-    .readdirSync(`./src/functions/${folder}`)
-    .filter((file) => file.endsWith(".js"));
-
+  .readdirSync(`./src/functions/${folder}`)
+  .filter((file) => file.endsWith(".js"));
+  
   for (const file of functionFiles)
-    require(`./functions/${folder}/${file}`)(client);
+  require(`./functions/${folder}/${file}`)(client);
 }
 
 client.handleEvents();
@@ -59,3 +69,7 @@ client.login(token);
   console.log(`Connected To Database ${databaseToken}`);
   await connect(databaseToken).catch(console.error);
 })();
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server Up on port 3000`)
+});
